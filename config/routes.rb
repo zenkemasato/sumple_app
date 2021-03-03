a2b68a5281e41b63b665f99dbdfbb113a9235df1
zenkemasato <zenke0821@icloud.com>
2021-03-03 13:31:41 +0000

    finied ch07

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get '/signup',   to: 'users#new'
-end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
