412f867ee1e078721e127e6d7fe2830d6276d61a
zenkemasato <zenke0821@icloud.com>
2021-03-07 01:56:55 +0000

    finished ch11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
@@ -48,7 +48,8 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-
+  
+  
   private
 
     def user_params
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+   config.action_mailer.raise_delivery_errors = false
+
+  host = 'https://8e6cc467f8d54d518430903ebb411738.vfs.cloud9.ap-northeast-1.amazonaws.com' # ここをコピペすると失敗します。自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
